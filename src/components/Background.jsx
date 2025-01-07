6d39daa3bfb8e778d05fd6f3c7741ae595ba9309
Rocco Vulpis <roccovulpis@gmail.com>
2025-01-07 17:12:37 -0500

    Structure change

---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Background.css'
+
+export default function Background() {
+    return (
+        <div class="bg-animation">
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div id="stars4"></div>
+        </div>
+    )
+}
