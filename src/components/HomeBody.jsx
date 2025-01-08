e51449ec724ef8fa085815085cffcf4bc9188bfb
Rocco Vulpis <roccovulpis@gmail.com>
2025-01-07 22:33:42 -0500

    Finished navbar and homepage card.

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import selfImg from '../assets/me.jpg'
 import '../styles/Homebody.css'
+import { FaLocationDot } from "react-icons/fa6";
+import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 export default function HomeBody() {
   return (
-    <div>
-      <div className='img-container'>
-        <img src={selfImg} alt="Me" />
+    <div className='body-container'>
+      <div className='body-card'>
+        <div className='img-container'>
+          <img src={selfImg} alt="Me" />
+        </div>
+        <div className='desc-container'>
+          <h1>Rocco Vulpis</h1>
+          <h2>Software Developer</h2>
+          <span><FaLocationDot /> New Jersey, USA</span>
+          <p className='desc-tag'>I created this website to showcase my skills and 
+            projects I've worked on in school and on my spare time.
+            </p>
+        </div>
+        <div>
+          <div className='socials-container'>
+            <h2>Socials</h2>
+            <span className='social-span'>
+              <button><CiLinkedin /></button>
+              <button><FaGithub /></button>
+            </span>
+          </div>
+
+        </div>
       </div>
-      <h1>Hi, I'm Rocco</h1>
-      <p>This is my portfolio</p>
-      <button>Check Out My Work</button>
 
     </div>
   )
